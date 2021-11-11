db43d6e6983c5b9c376f4e2c8764cc2f091871e2
Denis Zunke <denis.zunke@gmail.com>
2021-11-11 11:13:46 +0100

    Simpler loader screen events that also work with browser history

--- assets/controllers/loader_controller.js
+++ assets/controllers/loader_controller.js
@@ -1,82 +1,64 @@
-import { Controller } from 'stimulus';
+import {Controller} from 'stimulus';
 
 export default class extends Controller {
-    #activeLoaderId = 'active-loader-element'
+  #activeLoaderId = 'active-loader-element'
 
-    connect() {
-      const that = this;
+  connect() {
+    const that = this;
 
-      // window.onbeforeunload = function (e) {
-      // Removed due to double loading of loader and no-loader class on click is not working
-      // that.check();
-      // };
+    window.onbeforeunload = () => that.check();
+    window.onpageshow = () => that.unload();
+  }
 
-      document.querySelectorAll('a, button[type="submit"]').forEach((elem) => {
-        elem.addEventListener('click', ($event) => {
-          if (elem !== $event.target) return;
+  check($element) {
+    const $existingLoader = document.getElementById(this.activeLoaderId);
+    if ($existingLoader !== null) {
+      return;
+    }
 
-          let $element = $event.currentTarget;
-          if ($event.target.nodeName === 'I') {
-            $element = $event.currentTarget.parentNode;
-          }
+    if ($element !== undefined && $element.classList.contains('no-loader')) {
+      return;
+    }
 
-          that.check($element);
-        });
-      });
-
-      document.addEventListener('keydown', ($e) => {
-        if (($e.which || $e.keyCode) === 116) { // F5
-          that.check();
+    if ($element !== undefined) {
+      const $checkForm = $element.getAttribute('data-check-form');
+      if ($checkForm !== null) {
+        const $form = document.querySelector(`form[name="${$checkForm}"]`);
+        if ($form === null || $form.noValidate || $form.checkValidity()) {
+          this.load();
         }
-      });
-    }
 
-    check($element) {
-      const $existingLoader = document.getElementById(this.activeLoaderId);
-      if ($existingLoader !== null) {
         return;
       }
-
-      if ($element !== undefined && $element.classList.contains('no-loader')) {
-        return;
-      }
-
-      if ($element !== undefined) {
-        const $checkForm = $element.getAttribute('data-check-form');
-        if ($checkForm !== null) {
-          const $form = document.querySelector(`form[name="${$checkForm}"]`);
-          if ($form === null || $form.noValidate || $form.checkValidity()) {
-            this.load();
-          }
-
-          return;
-        }
-      }
-
-      this.load();
     }
 
-    load() {
-      if (document.getElementById(this.activeLoaderId) !== null) {
-        return;
-      }
+    this.load();
+  }
 
-      const $template = document.getElementById(this.templateId);
-      const $loaderElement = $template.content.cloneNode(true);
-      $loaderElement.querySelector('div').id = this.activeLoaderId;
-
-      document.body.append($loaderElement);
+  load() {
+    if (document.getElementById(this.activeLoaderId) !== null) {
+      return;
     }
 
-    unload() {
-      document.getElementById(this.activeLoaderId).remove();
-    }
+    const $template = document.getElementById(this.templateId);
+    const $loaderElement = $template.content.cloneNode(true);
+    $loaderElement.querySelector('div').id = this.activeLoaderId;
 
-    get templateId() {
-      return this.data.get('templateId');
-    }
+    document.body.append($loaderElement);
+  }
 
-    get activeLoaderId() {
-      return this.#activeLoaderId;
+  unload() {
+    const $element = document.getElementById(this.activeLoaderId);
+    if ($element !== null) {
+      $element.remove();
     }
+  }
+
+  get templateId() {
+    return this.data.get('templateId');
+  }
+
+  get activeLoaderId() {
+    return this.#activeLoaderId;
+  }
 }
--- assets/controllers/refresh_controller.js
+++ assets/controllers/refresh_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static refresh() {
+  refresh() { // eslint-disable-line class-methods-use-this
     const { location } = window;
     window.location = location;
   }
